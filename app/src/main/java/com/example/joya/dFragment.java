a615622568be7c4864988193682838f1d5522be1
maddy <maddymadhavvinod@gmail.com>
2021-07-25 17:19:03 +0530

    poda naye2

--- app/src/main/java/com/example/joya/dFragment.java
+++ app/src/main/java/com/example/joya/dFragment.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
