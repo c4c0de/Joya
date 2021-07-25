e6a05cfec665e98f32e359252983adf43560a04a
alenj <alenjacob.vinu@gmail.com>
2021-07-25 17:05:02 +0530

    clean code

---
+++ app/src/main/java/com/example/joya/dFragment.java
@@ -0,0 +1,30 @@
+package com.example.joya;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class dFragment extends Fragment {
+
+ 
+
+    public dFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_d, container, false);
+
+       
+
+
+    }
+}
