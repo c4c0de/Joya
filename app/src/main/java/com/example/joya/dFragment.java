25b1d2499bd714adb96c284266881656033fb7e3
alenj <alenjacob.vinu@gmail.com>
2021-07-25 16:25:02 +0530

    madhav

--- app/src/main/java/com/example/joya/dFragment.java
+++ app/src/main/java/com/example/joya/dFragment.java
@@ -1,5 +1,6 @@
 package com.example.joya;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class dFragment extends Fragment {
@@ -31,6 +34,13 @@ public class dFragment extends Fragment {
         btLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(getApplicationContext(), OTP_authentication.class));
+//                finish();
+                Intent i = new Intent();
+                i.setClass(getActivity(), OTP_authentication.class);
+                startActivity(i);
 
                 Toast.makeText(getActivity(), "hellooooooo", Toast.LENGTH_SHORT).show();
             }
